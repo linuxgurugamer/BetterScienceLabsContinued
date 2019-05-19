cdf6a18f302a2c81256d90e4f984c2be8c5c80ef
Jonathan Bayer <linuxgurugamer@gmail.com>
2019-05-19 14:42:14 -0400

    Updated for 1.7

---
+++ GameData/BetterScienceLabsContinued/BashCopyTextures.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#
+# This script needs to be run on Linux and OSX to copy
+# the missing texture files.  Double-clicking on it
+# should be sufficient
+
+clear
+
+echo -e "\n\nThis batch file will copy the missing textures from the inaccessable"
+echo "Squad/zDeprecated directory for the SXTLT80 part, also known as"
+echo "the LV-T80 "Robin" Liquid Fuel Engine"
+echo -e "\n\n"
+echo "Press return to continue"
+read yn
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+mkdir -p $DIR/Squad/Command
+cd $DIR/Squad/Command
+mkdir Mk1-2Pod
+cp ../../../Squad/zDeprecated/Parts/Command/Mk1-2Pod/* Mk1-2Pod
+
+
+echo -e "\n\nThe files have been copied\n"
+echo "Press return to continue"
+read yn
---
+++ GameData/BetterScienceLabsContinued/Windows_CopyTextures.bat
@@ -0,0 +1,28 @@
+rem
+rem This file should be run on Windows, in the Kerbonov directory
+rem
+rem Double-clicking on the file while in Windows Explorer should
+rem be sufficient
+
+@echo off
+echo.
+echo This batch file will copy the missing textures from the inaccessable
+echo Squad/zDeprecated directory for the Mk1-2Pod part, also known as
+echo the Mk 1-2 Pod
+echo.
+echo.
+pause
+
+mkdir Squad
+cd Squad
+mkdir Command
+cd Command
+mkdir Mk1-2Pod
+cd Mk1-2Pod
+copy ..\..\..\..\Squad\zDeprecated\Parts\Command\Mk1-2Pod\*
+
+echo.
+echo.
+echo The files have been copied
+echo.
+pause
